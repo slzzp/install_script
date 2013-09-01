7215bf5d6727d59acb863a837f890f741af8b848
slzzp <sleepgo@gmail.com>
2013-09-02 06:33:48 +0800

    add debian/install_php.sh

---
+++ debian/install_php.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# This script generates php cli + fpm + apache2 module
+
+# ref: http://www.php.net/get/php-5.5.3.tar.gz/from/a/mirror
+URLPHP='http://www.php.net/distributions/php-5.5.3.tar.gz'
+
+BASENAME='/usr/bin/basename'
+MAKE='/usr/bin/make'
+RM='/bin/rm'
+SED='/bin/sed'
+TAR='/bin/tar'
+WGET='/usr/bin/wget'
+
+# ----------------------------------------------------------------------
+
+FILEPHP=`${BASENAME} ${URLPHP}`
+DIRPHP=`echo -n ${FILEPHP} | ${SED} 's/\.tar\.gz//g'`
+
+cd /tmp
+
+# get source tarball
+if [ ! -f "${FILEPHP}" ]; then
+    ${WGET} -4 ${URLPHP}
+
+    if [ ! -f "${FILEPHP}" ]; then
+        echo "Sorry, can't get ${FILEPHP} for install php now."
+        exit
+    fi
+fi
+
+# remove old directory
+if [ -d "${DIRPHP}" ]; then
+    ${RM} -rf ${DIRPHP}
+fi
+
+# unpack source tarball
+${TAR} xzvf ${FILEPHP}
+
+
+# build and install
+cd ${DIRPHP}
+
+./configure --prefix=/service/php \
+  --enable-ctype \
+  --enable-fileinfo \
+  --enable-fpm \
+  --enable-mbstring \
+  --enable-sockets \
+  --with-apxs2=/service/apache2/bin/apxs \
+  --with-gd \
+  --with-gettext \
+  --with-mhash \
+  --with-mysql \
+  --with-pcre-regex \
+  --with-zlib
+#  --with-openssl \
+# bug ? OpenSSL 1.0.1e has no openssl/evp.h but php-5.5.3 --with-openssl need it
+
+${MAKE}
+${MAKE} install
