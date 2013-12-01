289ad0c9b84a1697592f680761b1f6ebfe727a91
slzzp <sleepgo@gmail.com>
2013-12-01 09:32:13 +0800

    update debian/install_apr-util.sh: use version 1.5.3

--- debian/install_apr-util.sh
+++ debian/install_apr-util.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ref: http://apr.apache.org/download.cgi?Preferred=http%3A%2F%2Fftp.twaren.net%2FUnix%2FWeb%2Fapache%2F
-URLAPRUTIL='http://ftp.twaren.net/Unix/Web/apache//apr/apr-util-1.5.2.tar.gz'
+URLAPRUTIL='http://ftp.twaren.net/Unix/Web/apache//apr/apr-util-1.5.3.tar.gz'
 
 BASENAME='/usr/bin/basename'
 MAKE='/usr/bin/make'
