6dd011476d62ba3eb39cb5ee473af5aa31f92602
slzzp <sleepgo@gmail.com>
2013-12-01 09:31:43 +0800

    update debian/install_apr.sh: use version 1.5.0

--- debian/install_apr.sh
+++ debian/install_apr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ref: http://apr.apache.org/download.cgi?Preferred=http%3A%2F%2Fftp.twaren.net%2FUnix%2FWeb%2Fapache%2F
-URLAPR='http://ftp.twaren.net/Unix/Web/apache//apr/apr-1.4.8.tar.gz'
+URLAPR='http://ftp.twaren.net/Unix/Web/apache//apr/apr-1.5.0.tar.gz'
 
 BASENAME='/usr/bin/basename'
 MAKE='/usr/bin/make'
