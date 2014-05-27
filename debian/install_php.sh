7ee979e6ffcd2454f23017ccddee32d3aff6640f
slzzp <sleepgo@gmail.com>
2014-05-27 18:31:35 +0800

    update debian/install_php.sh: add php settings to apache2; enable mod_proxy mod_proxy_fcgi mod_rewrite here

--- debian/install_php.sh
+++ debian/install_php.sh
@@ -6,9 +6,12 @@
 URLPHP='http://www.php.net/distributions/php-5.5.12.tar.gz'
 
 BASENAME='/usr/bin/basename'
+CAT='/bin/cat'
 CP='/bin/cp'
 GREP='/bin/grep'
 MAKE='/usr/bin/make'
+MKDIR='/bin/mkdir'
+MV='/bin/mv'
 PWD='/bin/pwd'
 RM='/bin/rm'
 SED='/bin/sed'
@@ -121,10 +124,128 @@ if [ ! -f '/service/php/etc/php-fpm.conf' ]; then
     ${CP} etc/php-fpm.conf /service/php/etc/
 fi
 
+# ----------------------------------------------------------------------
+
 CHECKCOUNT=`${GREP} /service/php/sbin/php-fpm /etc/rc.local | ${WC} -l | ${TR} -d ' '`
 if [ "0" = "${CHECKCOUNT}" ]; then
     echo 'Activate php-fpm in /etc/rc.local'
 
     ${TOUCH} /etc/rc.local
     echo '/service/php/sbin/php-fpm &' >> /etc/rc.local
+fi
+
+# ----------------------------------------------------------------------
+
+# check apache setting
+if [ ! -f '/service/apache2/conf/local_config/php' ]; then
+    echo 'Activate php settings to apache2'
+
+    ${MKDIR} -p /service/apache2/conf/local_config
+
+    TMPFILE="$0_tmpfile_$$"
+
+    if [ -f "${TMPFILE}" ]; then
+        ${RM} -f ${TMPFILE}
+    fi
+
+    ${WGET} -q -O ${TMPFILE} https://raw.github.com/slzzp/install_script/master/debian/apache_conf/local_config/php
+
+    if [ ! -f "${TMPFILE}" ]; then
+        echo "Sorry, can't get apache config file now, or current/working directory is forbidden to write."
+        exit
+    fi
+
+    ${MV} ${TMPFILE} /service/apache2/conf/local_config/php
+fi
+
+# ----------------------------------------------------------------------
+
+# mod_proxy for php-fpm
+TMPCOUNT=`${GREP} 'LoadModule proxy_module modules/mod_proxy.so' /service/apache2/conf/httpd.conf | ${WC} -l | ${TR} -d ' '`
+if [ "${TMPCOUNT}" = "0" -o ! -f '/service/apache2/modules/mod_proxy.so' ]; then
+    echo "Sorry, mod_proxy is not in apache."
+    exit
+fi
+
+TMPCOUNT=`${GREP} '#LoadModule proxy_module modules/mod_proxy.so' /service/apache2/conf/httpd.conf | ${WC} -l | ${TR} -d ' '`
+if [ "${TMPCOUNT}" = "0" ]; then
+    echo 'Activated mod_proxy in /service/apache2/conf/httpd.conf'
+else
+    if [ -f '/service/apache2/conf/httpd.conf.new' ]; then
+        ${RM} -f /service/apache2/conf/httpd.conf.new
+    fi
+
+    echo 'Activate mod_proxy in /service/apache2/conf/httpd.conf'
+
+    ${CAT} /service/apache2/conf/httpd.conf | \
+      ${SED} 's/\#LoadModule proxy_module modules\/mod_proxy\.so/LoadModule proxy_module modules\/mod_proxy\.so/g' > \
+      /service/apache2/conf/httpd.conf.new
+
+    ${MV} /service/apache2/conf/httpd.conf.new /service/apache2/conf/httpd.conf
+fi
+
+# ----------------------------------------------------------------------
+
+# mod_proxy_fcgi for php-fpm
+TMPCOUNT=`${GREP} 'LoadModule proxy_fcgi_module modules/mod_proxy_fcgi.so' /service/apache2/conf/httpd.conf | ${WC} -l | ${TR} -d ' '`
+if [ "${TMPCOUNT}" = "0" -o ! -f '/service/apache2/modules/mod_proxy_fcgi.so' ]; then
+    echo "Sorry, mod_proxy_fcgi is not in apache."
+    exit
+fi
+
+TMPCOUNT=`${GREP} '#LoadModule proxy_fcgi_module modules/mod_proxy_fcgi.so' /service/apache2/conf/httpd.conf | ${WC} -l | ${TR} -d ' '`
+if [ "${TMPCOUNT}" = "0" ]; then
+    echo 'Activated mod_proxy_fcgi in /service/apache2/conf/httpd.conf'
+else
+    if [ -f '/service/apache2/conf/httpd.conf.new' ]; then
+        ${RM} -f /service/apache2/conf/httpd.conf.new
+    fi
+
+    echo 'Activate mod_proxy_fcgi in /service/apache2/conf/httpd.conf'
+
+    ${CAT} /service/apache2/conf/httpd.conf | \
+      ${SED} 's/\#LoadModule proxy_fcgi_module modules\/mod_proxy_fcgi\.so/LoadModule proxy_fcgi_module modules\/mod_proxy_fcgi\.so/g' > \
+      /service/apache2/conf/httpd.conf.new
+
+    ${MV} /service/apache2/conf/httpd.conf.new /service/apache2/conf/httpd.conf
+fi
+
+# ----------------------------------------------------------------------
+
+# mod_rewrite for Zend Framework / PixFramework
+TMPCOUNT=`${GREP} 'LoadModule rewrite_module modules/mod_rewrite.so' /service/apache2/conf/httpd.conf | ${WC} -l | ${TR} -d ' '`
+if [ "${TMPCOUNT}" = "0" -o ! -f '/service/apache2/modules/mod_rewrite.so' ]; then
+    if [ ! -f '/service/apache2/modules/mod_rewrite.so' ]; then
+        echo "Sorry, mod_rewrite is not in apache."
+        exit
+    fi
+
+    if [ -f '/service/apache2/conf/httpd.conf.new' ]; then
+        ${RM} -f /service/apache2/conf/httpd.conf.new
+    fi
+
+    echo 'Activate mod_rewrite in /service/apache2/conf/httpd.conf'
+
+    ${CAT} /service/apache2/conf/httpd.conf | \
+      ${SED} 's/LoadModule alias_module modules\/mod_alias\.so/LoadModule alias_module modules\/mod_alias\.so\nLoadModule rewrite_module modules\/mod_rewrite\.so/g' > \
+      /service/apache2/conf/httpd.conf.new
+
+    ${MV} /service/apache2/conf/httpd.conf.new /service/apache2/conf/httpd.conf
+fi
+
+TMPCOUNT=`${GREP} '#LoadModule rewrite_module modules/mod_rewrite.so' /service/apache2/conf/httpd.conf | ${WC} -l | ${TR} -d ' '`
+if [ "${TMPCOUNT}" = "0" ]; then
+    echo 'Activated mod_rewrite in /service/apache2/conf/httpd.conf'
+else
+    if [ -f '/service/apache2/conf/httpd.conf.new' ]; then
+        ${RM} -f /service/apache2/conf/httpd.conf.new
+    fi
+
+    echo 'Activate mod_rewrite in /service/apache2/conf/httpd.conf'
+
+    ${CAT} /service/apache2/conf/httpd.conf | \
+      ${SED} 's/\#LoadModule rewrite_module modules\/mod_rewrite\.so/LoadModule rewrite_module modules\/mod_rewrite\.so/g' > \
+      /service/apache2/conf/httpd.conf.new
+
+    ${MV} /service/apache2/conf/httpd.conf.new /service/apache2/conf/httpd.conf
 fi
