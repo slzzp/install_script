a655477542033b15ac5cdc5e2f8d5796b431abff
slzzp <sleepgo@gmail.com>
2014-05-29 17:39:02 +0800

    update debian/install_apache.sh: add config/sites settings; style

--- debian/install_apache.sh
+++ debian/install_apache.sh
@@ -3,9 +3,17 @@
 # ref: http://httpd.apache.org/download.cgi?Preferred=http%3A%2F%2Fftp.twaren.net%2FUnix%2FWeb%2Fapache%2F
 URLAPACHE='http://ftp.twaren.net/Unix/Web/apache//httpd/httpd-2.4.9.tar.gz'
 
+AWK='/usr/bin/awk'
 BASENAME='/usr/bin/basename'
+CAT='/bin/cat'
+CUT='/usr/bin/cut'
 GREP='/bin/grep'
+HEAD='/usr/bin/head'
+HOSTNAME='/bin/hostname'
+IFCONFIG='/sbin/ifconfig'
 MAKE='/usr/bin/make'
+MKDIR='/bin/mkdir'
+MV='/bin/mv'
 RM='/bin/rm'
 SED='/bin/sed'
 TAR='/bin/tar'
@@ -65,7 +73,8 @@ ${TAR} xzvf ${FILEAPACHE}
 # build
 cd ${DIRAPACHE}
 
-./configure --prefix=/service/apache2 \
+./configure \
+  --prefix=/service/apache2 \
   --enable-rewrite \
   --enable-ssl \
   --enable-so \
@@ -84,6 +93,156 @@ if [ "0" = "${CHECKCOUNT}" ]; then
 
     ${TOUCH} /etc/rc.local
     echo '/service/apache2/bin/apachectl start &' >> /etc/rc.local
+else
+    echo 'Activated apache2 in /etc/rc.local'
+fi
+
+# ----------------------------------------------------------------------
+
+TMPFILE="$0_tmpfile_$$"
+
+if [ ! -d '/service/apache2/conf/local_config' ]; then
+    echo 'Build /service/apache2/conf/local_config'
+
+    ${MKDIR} /service/apache2/conf/local_config
+else
+    echo 'Found /service/apache2/conf/local_config'
+fi
+
+TMPCOUNT=`${GREP} conf/local_config /service/apache2/conf/httpd.conf | ${WC} -l | ${TR} -d ' '`
+if [ "${TMPCOUNT}" = "0" ]; then
+    if [ -f "${TMPFILE}" ]; then
+        ${RM} -f ${TMPFILE}
+    fi
+
+    ${WGET} -q -O ${TMPFILE} https://raw.githubusercontent.com/slzzp/install_script/master/debian/apache_conf/httpd.conf_append_local_config
+
+    if [ ! -f "${TMPFILE}" ]; then
+        echo "Sorry, can't get apache append file now, or current/working directory is forbidden to write."
+        exit
+    fi
+
+    echo 'Activate conf/local_config to /service/apache2/conf/httpd.conf'
+
+    ${CAT} ${TMPFILE} >> /service/apache2/conf/httpd.conf
+else
+    echo 'Activated conf/local_config to /service/apache2/conf/httpd.conf'
+fi
+
+# ----------------------------------------------------------------------
+
+if [ ! -d '/service/apache2/conf/local_sites' ]; then
+    echo 'Build /service/apache2/conf/local_sites'
+
+    ${MKDIR} /service/apache2/conf/local_sites
+else
+    echo 'Found /service/apache2/conf/local_sites'
+fi
+
+TMPCOUNT=`${GREP} conf/local_sites /service/apache2/conf/httpd.conf | ${WC} -l | ${TR} -d ' '`
+if [ "${TMPCOUNT}" = "0" ]; then
+    if [ -f "${TMPFILE}" ]; then
+        ${RM} -f ${TMPFILE}
+    fi
+
+    ${WGET} -q -O ${TMPFILE} https://raw.githubusercontent.com/slzzp/install_script/master/debian/apache_conf/httpd.conf_append_local_sites
+
+    if [ ! -f "${TMPFILE}" ]; then
+        echo "Sorry, can't get apache append file now, or current/working directory is forbidden to write."
+        exit
+    fi
+
+    echo 'Activate conf/local_sites to /service/apache2/conf/httpd.conf'
+
+    ${CAT} ${TMPFILE} >> /service/apache2/conf/httpd.conf
+else
+    echo 'Activated conf/local_sites to /service/apache2/conf/httpd.conf'
+fi
+
+# ----------------------------------------------------------------------
+
+if [ ! -f '/service/apache2/conf/local_config/charset' ]; then
+    if [ -f "${TMPFILE}" ]; then
+        ${RM} -f ${TMPFILE}
+    fi
+
+    ${WGET} -q -O ${TMPFILE} https://raw.githubusercontent.com/slzzp/install_script/master/debian/apache_conf/local_config/charset
+
+    if [ ! -f "${TMPFILE}" ]; then
+        echo "Sorry, can't get apache config file now, or current/working directory is forbidden to write."
+        exit
+    fi
+
+    echo 'Build charset to /service/apache2/conf/local_config'
+
+    ${MV} ${TMPFILE} /service/apache2/conf/local_config/charset
+else
+    echo 'Found charset to /service/apache2/conf/local_config'
+fi
+
+if [ ! -f '/service/apache2/conf/local_config/security' ]; then
+    if [ -f "${TMPFILE}" ]; then
+        ${RM} -f ${TMPFILE}
+    fi
+
+    ${WGET} -q -O ${TMPFILE} https://raw.githubusercontent.com/slzzp/install_script/master/debian/apache_conf/local_config/security
+
+    if [ ! -f "${TMPFILE}" ]; then
+        echo "Sorry, can't get apache config file now, or current/working directory is forbidden to write."
+        exit
+    fi
+
+    echo 'Build security to /service/apache2/conf/local_config'
+
+    ${MV} ${TMPFILE} /service/apache2/conf/local_config/security
+else
+    echo 'Found security to /service/apache2/conf/local_config'
+fi
+
+if [ ! -f '/service/apache2/conf/local_sites/default' ]; then
+    if [ -f "${TMPFILE}" ]; then
+        ${RM} -f ${TMPFILE}
+    fi
+
+    ${WGET} -q -O ${TMPFILE} https://raw.githubusercontent.com/slzzp/install_script/master/debian/apache_conf/local_sites/default
+
+    if [ ! -f "${TMPFILE}" ]; then
+        echo "Sorry, can't get apache site file now, or current/working directory is forbidden to write."
+        exit
+    fi
+
+
+    MYHOSTNAME=`${HOSTNAME}`
+    # usually use eth0
+    MYHOSTIP=`${IFCONFIG} eth0 | ${GREP} 'inet addr' | ${AWK} '{printf("%s",$2);}' | ${AWK} -F\: '{printf("%s",$2);}' `
+
+    if [ -z "${MYHOSTNAME}" ]; then
+        echo "Sorry, can't get hostname. Please check /etc/hostname setting."
+        exit
+    fi
+
+    if [ -z "${MYHOSTIP}" ]; then
+        echo "Sorry, can't get host ip. Please check /etc/network/interfaces setting, or fix this scirpt if you are not using eth0."
+        exit
+    fi
+
+    echo "Build default to /service/apache2/conf/local_config using hostname '${MYHOSTNAME}' and host ip '${MYHOSTIP}'"
+
+    ${CAT} ${TMPFILE} | \
+      ${SED} "s/MYHOSTNAME/${MYHOSTNAME}/g" | \
+      ${SED} "s/MYHOSTIP/${MYHOSTIP}/g" > \
+      /service/apache2/conf/local_sites/default
+
+    ${MKDIR} -p /service/apache2/logs/default
+else
+    echo 'Found default to /service/apache2/conf/local_config'
+fi
+
+# ----------------------------------------------------------------------
+
+# remove temp file
+if [ -f "${TMPFILE}" ]; then
+    ${RM} -f ${TMPFILE}
 fi
 
 # ----------------------------------------------------------------------
