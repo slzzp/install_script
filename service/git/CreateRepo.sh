f7a65aae0c45e99738aae29dde6802e57508b44c
Chien-Ta Wu <sleepgo@gmail.com>
2015-05-02 08:08:36 +0800

    add service/git/CreateRepo.sh

---
+++ service/git/CreateRepo.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# run as user 'root'
+
+CHOWN=/usr/sbin/chown
+GIT=/usr/local/bin/git
+MKDIR=/bin/mkdir
+WHOAMI=/usr/bin/whoami
+
+
+if [ `${WHOAMI}` != 'root' ]; then
+  echo "Error: you must be root."
+  exit
+fi
+
+if [ -z "$1" ]; then
+  echo "Error: no given repo name."
+  exit
+fi
+
+cd ~git/repo
+
+if [ -d "$1" -o -d "$1.git" ]; then
+  echo "Error: repo '$1' is already exist."
+  exit
+fi
+
+
+# build repo
+
+${MKDIR} "$1"
+
+cd "$1"
+
+${GIT} init --bare --shared
+
+${CHOWN} -R git:git .
